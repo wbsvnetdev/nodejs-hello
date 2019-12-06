105ceba622be9888c4dfccd14c0f2cbdf5b7cc67
Wilfried BESSOVI <w.bessovi@linkbynet.com>
2019-12-06 15:40:55 +0100

    First Shot

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+
+  extended: true
+
+}));
+
+console.log("Hello Treeptik")
+
+app.get("/", function(req, res){
+    res.render("home")
+})
+
+app.listen(3000, function(){
+  console.log("Server listening on port 3000")
+})
+
+
